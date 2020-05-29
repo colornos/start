6a79a7f981e9c78638fb078b231eb2ef0fcb6b64
colornos <rivas432@hotmail.com>
2020-05-29 01:59:44 -0500

    Update count.py

--- count.py
+++ count.py
@@ -5,27 +5,17 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+blue = LED(16)
+red = LED(27)
+green = LED(22)
+count = 0
+
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
+	count = count + 1
         print('Button Pressed')
-        time.sleep(0.5)
-
-	f1=open("one.txt", "w")
-	f1.write("0")
-	f1.close()
-
-        f2=open("two.txt", "w")
-        f2.write("0")
-	f2.close()
-
-        f3=open("three.txt", "w")
-        f3.write("0")
-	f3.close()
-
-        f4=open("four.txt", "w")
-        f4.write("0")
-	f4.close()
+        time.sleep(2.5)
 
         #Configure LED Output Pin
         LED = 16
@@ -33,7 +23,12 @@ while True:
         GPIO.output(LED, GPIO.LOW)
 
         GPIO.output(LED, GPIO.HIGH)  #Turn on LED
-        time.sleep(5)                #Wait 5 Seconds
+        time.sleep(2)                #Wait 5 Seconds
         GPIO.output(LED, GPIO.LOW)   #Turn off LED
-
-        print "NFC reset"
+	if count = 2:
+		print "pressed twice"
+	if count = 3:
+		print "pressed three times"
+	if count = 4:
+		print "pressed four times"
+	
