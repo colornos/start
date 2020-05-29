5218714a57a288f8536ac0113dcade957b295042
colornos <rivas432@hotmail.com>
2020-05-29 00:26:19 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,39 +1,28 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import LED, Button
+from signal import pause
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+blue = LED(17)
+red = LED(27)
+green = LED(22)
+button = Button(2)
+count = 0
+
 
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.5)
-
-	f1=open("one.txt", "w")
-	f1.write("0")
-	f1.close()
-
-        f2=open("two.txt", "w")
-        f2.write("0")
-	f2.close()
-
-        f3=open("three.txt", "w")
-        f3.write("0")
-	f3.close()
-
-        f4=open("four.txt", "w")
-        f4.write("0")
-	f4.close()
-
-        #Configure LED Output Pin
-        LED = 16
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.output(LED, GPIO.LOW)
-
-        GPIO.output(LED, GPIO.HIGH)  #Turn on LED
-        time.sleep(5)                #Wait 5 Seconds
-        GPIO.output(LED, GPIO.LOW)   #Turn off LED
-
-        print "NFC reset"
+    button.wait_for_press()
+    blue.on()
+    
+    button.wait_for_release()
+    blue.off()
+    
+    count = count + 1
+    if count == 3:
+        red.on()
+    elif count == 4:
+        red.off()
+        green.on()
+    elif count == 5:
+        count = 0
+        green.off()
+    print(count)
