09b340aba8b7f5e98c7b1cea4058b9c3a5333429
colornos <rivas432@hotmail.com>
2020-05-29 02:37:56 -0500

    Update count.py

--- count.py
+++ count.py
@@ -1,25 +1,44 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(18,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 count = 0
 
 while True:
-    inputValue = GPIO.input(18)
-    if (inputValue == True):
-	count = count + 1
-        print("Button Pressed " + str(count) + " times.")
-        time.sleep(.5)
-    time.sleep(.5)
-
-        #Configure LED Output Pin
-        #LED = 16
-        #GPIO.setup(LED, GPIO.OUT)
-        #GPIO.output(LED, GPIO.LOW)
-
-        #GPIO.output(LED, GPIO.HIGH)  #Turn on LED
-        #time.sleep(2)                #Wait 5 Seconds
-        #GPIO.output(LED, GPIO.LOW)   #Turn off LED
+    while GPIO.input(18) == 1:
+        sleep(0.2)
+      
+    count = count + 1
+    GPIO.output(16,GPIO.HIGH)
+    
+    while GPIO.input(18) == 0:
+        sleep(0.2)   
+         
+    GPIO.output(16,GPIO.LOW)
+    print(count)
+    
+    if count == 2:
+        GPIO.output(26,GPIO.HIGH)
+        sleep(1)
+        GPIO.output(26,GPIO.LOW)
+        GPIO.output(5,GPIO.HIGH)
+        sleep(1)
+        GPIO.output(5,GPIO.LOW)
+        
+    elif count == 3:
+        GPIO.output(26,GPIO.HIGH)
+        
+    elif count == 4:
+        GPIO.output(26,GPIO.LOW)
+        GPIO.output(8,GPIO.HIGH)
+    elif count == 5:
+        count = 0
+        GPIO.output(8,GPIO.LOW)
