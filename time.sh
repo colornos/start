6767b15a95b1b09ad6aa16a5bd20fe1a07f4a142
colornos <rivas432@hotmail.com>
2020-06-06 14:51:57 -0500

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-while true;
+runtime="5 minute"
+endtime=$(date -ud "$runtime" +%s)
+
+while [[ $(date -u +%s) -le $endtime ]]
 do
-    DATE=`date | cut -d' ' -f4`
-    echo $DATE
-    if [[ $DATE == "11:33:00" ]]
-    then
-            echo "this is a test program"
-            sleep 1s
-    fi
+    echo "Time Now: `date +%H:%M:%S`"
+    echo "Sleeping for 1 minute"
+    sleep 1m
 done
